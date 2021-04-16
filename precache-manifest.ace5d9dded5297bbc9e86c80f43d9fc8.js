d952cb3cf719f06aaf8bf27d8b5410151d4c9bcf
Harovinkin <kharovinkin@gmail.com>
2021-04-16 11:14:01 +0300

    Updates

---
+++ precache-manifest.ace5d9dded5297bbc9e86c80f43d9fc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62dde2c3817ce1799161d0c7b40952b1",
+    "url": "/Harovinkin/employees-list-search-app/index.html"
+  },
+  {
+    "revision": "b4a53fa4b5380b63c180",
+    "url": "/Harovinkin/employees-list-search-app/static/js/2.fc95608b.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Harovinkin/employees-list-search-app/static/js/2.fc95608b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0235c2e50bdfc188dd92",
+    "url": "/Harovinkin/employees-list-search-app/static/js/main.b2ba4b02.chunk.js"
+  },
+  {
+    "revision": "403eff42aad207f145a3",
+    "url": "/Harovinkin/employees-list-search-app/static/js/runtime-main.a9689e42.js"
+  }
+]);
